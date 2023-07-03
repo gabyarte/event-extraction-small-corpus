acf3d0cc4901931533c2d327cf98e41d9367a6f4
Gabriela Argüelles Terrón <gabyarte2@gmail.com>
2023-07-03 21:35:35 +0200

    feat: Process fine-tuning data for training and evaluation

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -32,9 +32,9 @@ def get_dict_from_data(sentences):
     sentence_pattern = r'\<\w+\>|\</\w+\>'
     patterns = {
         'subject': r'(?<=\<e1\>)(.+)(?=\</e1\>)',
-        'object': r'(?<=\<e2\>)(.+)(?=\</e2\>)',
+        'object': r'(?<=\<e2\>)(.*?)(?=\</e2\>)',
         'event': r'(?<=\<rel\>)(.+)(?=\</rel\>)',
-        'complement': r'(?<=\<comp\>)(.+)(?=\</comp\>)',
+        'complement': r'(?<=\<comp\>)(.*?)(?=\</comp\>)',
         'relationSignature': r'(?<=RelationSignature:\s)(.+)(?=\s\()',
         'relationType': r'(?<=RelationType:\s)(.+)(?=\s\()'
     }
@@ -47,8 +47,12 @@ def get_dict_from_data(sentences):
         sentence_data['text'] = ''.join(re.split(sentence_pattern, sentence))
 
         for role, pattern in patterns.items():
-            role_text = re.search(pattern, raw_sentence)
-            role_text = role_text and role_text.group(0)
+            role_text = re.findall(pattern, raw_sentence)
+            role_text = (
+                role_text and role_text[0]
+                if role not in ('complement', 'object') else
+                role_text or []
+            )
             if role != 'relationSignature':
                 sentence_data[role] = role_text
             else:
@@ -63,3 +67,46 @@ def get_dict_from_data(sentences):
         data.append(sentence_data)
 
     return data
+
+
+def prepare_data_for_fine_tune(data):
+    fine_tune_data = []
+
+    for sentence in data:
+        text = sentence['text']
+
+        fine_tune_prompt = {
+            'prompt': text + '\n\n###\n\n'
+        }
+
+        completion_data = []
+        if sentence["subject"]:
+            subject = sentence["subject"]
+            subject_item = f'{subject}/subject/{sentence["subjectLabel"]}'
+            completion_data.append((subject_item, text.index(subject)))
+
+        if sentence["object"]:
+            for object in sentence["object"]:
+                object_item = f'{object}/object/{sentence["objectLabel"]}'
+                completion_data.append((object_item, text.index(object)))
+
+        if sentence["event"]:
+            event = sentence["event"]
+            event_item = f'{event}/trigger/{sentence["relationType"]}'
+            completion_data.append((event_item, text.index(event)))
+
+        if sentence["complement"]:
+            for complement in sentence["complement"]:
+                complement_item = f'{complement}/complement/'
+                completion_data.append(
+                    (complement_item, text.index(complement)))
+
+        completion_data = sorted(completion_data, key=lambda x: x[1])
+
+        fine_tune_prompt['completion'] = ' ' + '\n'.join(
+            entity for entity, _ in completion_data
+        ) + '\n###'
+
+        fine_tune_data.append(fine_tune_prompt)
+
+    return fine_tune_data
